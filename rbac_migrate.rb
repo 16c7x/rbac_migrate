a86f518cf8e97f97d987c83b32777a7e8a4d34f1
Andrew Jones <andrew.jones@puppet.com>
2023-02-13 18:23:42 +0000

    almost working

--- rbac_migrate.rb
+++ rbac_migrate.rb
@@ -13,23 +13,31 @@ def get_rbac(token, hostname, filename)
 end
 
 def push_rbac(token, hostname, filename)
+  removes = ['Administrators', 'Operators', 'Viewers', 'Code Deployers', 'Project Deployers']
+  
   uri = URI.parse("https://" + hostname.strip + ":4433/rbac-api/v1/roles?token=#{token}")
 
   file = File.read(filename.strip)
-  data_hash = JSON.parse(file)
+  data_hash = JSON.parse(file, {:symbolize_names => true})
+  
+  # Clean out the unneeded roles
+  removes.each { |remove| data_hash.delete_if { |hash| hash[:display_name] == remove}} 
 
   header = {'Content-Type': 'application/json'}
 
   # Create the HTTP objects
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Post.new(uri.request_uri, header)
-  request.body = data_hash[5].to_json
+  
+  for index in 0 ... data_hash.size
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    request.body = data_hash[index].to_json
 
-  # Send the request
-  response = http.request(request)
-
+    # Send the request
+    response = http.request(request)
+    puts response
+  end
 end
 
 puts "Enter the API token: "
