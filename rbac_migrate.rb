ff74793c906859875ec6c1e090ec3adbdf0c2fe5
Andrew Jones <andrew.jones@puppet.com>
2023-02-14 12:11:03 +0000

    better output

--- rbac_migrate.rb
+++ rbac_migrate.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'net/http'
 require 'uri'
 require 'json'
-require 'pry'
+#require 'pry' # For debugging 
 
 def get_rbac(token, hostname, filename)
   response = open("https://" + hostname.strip + ":4433/rbac-api/v1/roles?token=#{token}", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
@@ -16,6 +16,7 @@ def push_rbac(token, hostname, filename)
   removes = ['Administrators', 'Operators', 'Viewers', 'Code Deployers', 'Project Deployers']
   
   uri = URI.parse("https://" + hostname.strip + ":4433/rbac-api/v1/roles?token=#{token}")
+  header = {'Content-Type': 'application/json'}
 
   file = File.read(filename.strip)
   data_hash = JSON.parse(file, {:symbolize_names => true})
@@ -23,10 +24,7 @@ def push_rbac(token, hostname, filename)
   # Clean out the unneeded roles
   removes.each { |remove| data_hash.delete_if { |hash| hash[:display_name] == remove}} 
 
-  header = {'Content-Type': 'application/json'}
-
-  # Create the HTTP objects
-  
+  # Create the HTTP objects  
   for index in 0 ... data_hash.size
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -34,9 +32,13 @@ def push_rbac(token, hostname, filename)
     request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = data_hash[index].to_json
 
+    # Blank the user and group id's to avoid conflict
+    data_hash[index][:user_ids]=[]
+    data_hash[index][:group_ids]=[]
+
     # Send the request
     response = http.request(request)
-    puts response
+    puts "RBAC grooup #{data_hash[index][:display_name]} status #{response}"
   end
 end
 
